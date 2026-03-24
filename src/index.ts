91d686c8adb5495d6f7c25e86a1f57abe5cae8b5
Aleksei Babushkin <babushkin.a.g.unity@gmail.com>
2026-03-24 13:08:09 +0400

    chore: setup pixi runtime with vite and vscode launch config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { bootstrap } from './app/bootstrap';
 
-void bootstrap();
+const container = document.getElementById('app');
+
+if (!container) {
+  throw new Error('Root container #app was not found.');
+}
+
+void bootstrap(container);
